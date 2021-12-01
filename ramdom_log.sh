827c424074590cdbca99f154d1e20d133aa9aff7
Yavor Tomov <yavoritomov@gmail.com>
2021-12-01 18:00:39 +0200

    adding rate per min setting. adding the value to each message.

--- ramdom_log.sh
+++ ramdom_log.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
+events_per_min='6000'
 while 'true'
 do
-   events_per_min='60'
    waitTime=$(bc <<< "scale=2; 60/$events_per_min")
-   sleep $waitTime &
+   sleep "$waitTime" &
    wait $!
    instruction=$(shuf -i 0-4 -n 1)
    d=$(date -Iseconds)
    case "$instruction" in
-      "1") echo "{\"@timestamp\": \"$d\", \"level\": \"ERROR\", \"message\": \"something happened in this execution.\"}"
+      "1") echo "{\"@timestamp\": \"$d\", \"level\": \"ERROR\", \"message\": \"something happened in this execution.\", \"rate\": \"Current rate of events(logs) $events_per_min per min.\"}"
       ;;
-      "2") echo "{\"@timestamp\": \"$d\", \"level\": \"INFO\", \"message\": \"takes the value and converts it to string.\"}"
+      "2") echo "{\"@timestamp\": \"$d\", \"level\": \"INFO\", \"message\": \"takes the value and converts it to string.\", \"rate\": \"Current rate of events(logs) $events_per_min per min.\"}"
       ;;
-      "3") echo "{\"@timestamp\": \"$d\", \"level\": \"WARN\", \"message\": \"variable not in use.\"}"
+      "3") echo "{\"@timestamp\": \"$d\", \"level\": \"WARN\", \"message\": \"variable not in use.\", \"rate\": \"Current rate of events(logs) $events_per_min per min.\"}"
       ;;
-      "4") echo "{\"@timestamp\": \"$d\", \"level\": \"DEBUG\", \"message\": \"first loop completed.\"}"
+      "4") echo "{\"@timestamp\": \"$d\", \"level\": \"DEBUG\", \"message\": \"first loop completed.\", \"rate\": \"Current rate of events(logs) $events_per_min per min.\"}"
       ;;
    esac
 done
